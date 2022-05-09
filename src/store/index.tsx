ad1b5933ff886ffdf5676499eaef489caa6daa72
wandaxoor <wanda.peruzzo@xoor.io>
2022-05-09 00:28:06 -0300

    change pass validate

---
+++ src/store/index.tsx
@@ -0,0 +1,44 @@
+import { v4 as uuidv4 } from 'uuid';
+import create from 'zustand';
+
+interface UserState {
+  users: User[];
+  addUser: (fullname: string) => void;
+  removeUser: (id: string) => void;
+  toggleCompletedState: (id: string) => void;
+}
+interface User {
+  id: string;
+  fullname: string;
+  mail: string;
+}
+
+export const useStore = create<UserState>(set => ({
+  // initial state
+  users: [],
+  // methods for manipulating state
+  addUser: (description: string) => {
+    set(state => ({
+      users: [
+        ...state.users,
+        {
+          id: uuidv4(),
+          fullname: '',
+          mail: '',
+        } as User,
+      ],
+    }));
+  },
+  removeUser: id => {
+    set(state => ({
+      users: state.users.filter(user => user.id !== id),
+    }));
+  },
+  toggleCompletedState: id => {
+    set(state => ({
+      users: state.users.map(user =>
+        user.id === id ? ({ ...user, completed: !user } as User) : user,
+      ),
+    }));
+  },
+}));
